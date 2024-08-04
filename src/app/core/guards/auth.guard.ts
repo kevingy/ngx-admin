c1f805b8159b1abbf1bd96e39a7a36df52aca59f
Kevin Yochum <kevin@bytewerx.com>
2024-08-03 20:42:11 -0500

    Update to Angular 16

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(
     private auth: AuthService,
--- src/app/core/guards/logged.guard.ts
+++ src/app/core/guards/logged.guard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Router, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
-export class Logged implements Resolve<any> {
+export class Logged  {
 
   constructor(
     private authService: AuthService,
