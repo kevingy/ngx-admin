a5595f85be569ad8dc3fbfeb79b75749f9c80b3d
Kevin Yochum <kevin@bytewerx.com>
2024-08-04 18:58:10 -0500

    Update to Angular 18

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppInterceptor } from './http/http.interceptor';
 import { Oauth2Interceptor } from './http/oauth2.interceptor';
 import { NotFoundComponent } from './layouts/not-found/not-found.component';
 
-@NgModule({
-  declarations: [
-    NotFoundComponent,
-  ],
-  imports: [
-    BrowserAnimationsModule, // For AngularMaterial Components
-    HttpClientModule,
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: Oauth2Interceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true }
-  ],
-  exports: []
-})
+@NgModule({ declarations: [
+        NotFoundComponent,
+    ],
+    exports: [], imports: [BrowserAnimationsModule], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: Oauth2Interceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class CoreModule { }
